1509014b81a7c6e8fce3d5b642edb2aff1c992ce
NisalDeZoysa <90953837+NisalDeZoysa@users.noreply.github.com>
2024-06-20 18:11:26 +0530

    adding stramlit chats correctly

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import yfinance as yf
 start = '2013-02-08'
 end = '2018-02-07'
 
-st.title('Stock market Prediction')
+st.title('Stock market Prediction of TESLA stocks')
 
 user_input = 'AAPL'
 
@@ -25,8 +25,8 @@ plt.plot(df.Close)
 st.pyplot(fig,'b')
 
 st.subheader('Closing Price vs Time Chart with 100MA and 200MA')
-ma100 = df.close.rolling(100).mean()
-ma100 = df.close.rolling(200).mean()
+ma100 = df['Close'].rolling(window=100).mean()
+ma200 = df['Close'].rolling(window=200).mean()
 fig = plt.figure(figsize=(12,6))
 plt.plot(ma100,'r')
 plt.plot(ma200,'g')
